6ee574c422c1a2f72e23e59b68994cecfd89cc6f
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-06-26 11:13:07 +0300

    life cycle

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -8,11 +8,49 @@ const PLACES = [
   { name: "Honolulu", zip: "96803" },
 ];
 class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null,
+    };
+  }
+  //Методы жизненного цикла позволяют нам написать
+  //дополнительный код, который вызывается в
+  //определенное время жизни компонента.
+  //мы хотим вызвать API, когда компонент загрузится
+  //на экране, для этого добавим componentDidMount.
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      zip +
+      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(URL).then((res) =>
+      res.json().then((json) => {
+        this.setState({ weatherData: json });
+      })
+    );
+  }
   render() {
+    const weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    const weather = weatherData.weather[0];
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    // return <div>{JSON.stringify(weatherData)}</div>;
     return (
-      <h1>
-        Displaying Weather for city {this.props.name} zip: {this.props.zip}
-      </h1>
+      // <h1>
+      //   Displaying Weather for city {this.props.name} zip: {this.props.zip}
+      // </h1>
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description} />
+        </h1>
+        <p>Current: {weatherData.main.temp}°</p>
+        <p>High: {weatherData.main.temp_max}°</p>
+        <p>Low: {weatherData.main.temp_min}°</p>
+        <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+      </div>
     );
   }
 }
