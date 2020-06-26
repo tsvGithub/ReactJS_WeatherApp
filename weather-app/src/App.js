0204cc45b337caf04b6f75639b871b7bcb362021
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-06-26 10:52:03 +0300

    state

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const places = [
+const PLACES = [
   { name: "Palo Alto", zip: "94303" },
   { name: "San Jose", zip: "94088" },
   { name: "Santa Cruz", zip: "95062" },
@@ -9,25 +9,55 @@ const places = [
 ];
 class WeatherDisplay extends Component {
   render() {
-    return <h1>Displaying Weather for city {this.props.zip}</h1>;
+    return (
+      <h1>
+        Displaying Weather for city {this.props.name} zip: {this.props.zip}
+      </h1>
+    );
   }
 }
 
 class App extends Component {
+  //Мы хотим, чтобы в приложении была возможность
+  //переключаться между местами, поэтому мы можем
+  //использовать состояние (state) для хранения данных в
+  //компоненте App.
+  //----------------------
+  //Сначала добавим функцию-конструктор, в которой будет
+  //использоваться super(), а затем установим начальное
+  //состояние this.state:
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0,
+    };
+  }
   render() {
+    //Функция render() может брать данные из this.state
+    //при составлении пользовательского интерфейса. Для
+    //этого мы можем использовать метод setState
+    //компонента React, который меняет состояние и
+    //перезапускает функцию render().
+    //--------------------
+    //Применим this.state и this.setState
+    const activePlace = this.state.activePlace;
+
     return (
       <div className="App">
-        {places.map((place, index) => (
+        {/* Из массива данных мы создадим набор элементов 
+        button и назначим свойство key для каждого, чтобы 
+        React знал последовательность элементов в массиве.*/}
+        {PLACES.map((place, index) => (
           <button
             key={index}
             onClick={() => {
-              console.log("Clicked index " + index);
+              this.setState({ activePlace: index });
             }}
           >
             {place.name}
           </button>
         ))}
-        <WeatherDisplay zip={"12345"} />
+        <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} name={PLACES[activePlace].name} />
       </div>
     );
   }
