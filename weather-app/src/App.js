2a997e57cab5213ea778256033a6c374426c1958
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-06-26 10:26:14 +0300

    props

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const places = [
+  { name: "Palo Alto", zip: "94303" },
+  { name: "San Jose", zip: "94088" },
+  { name: "Santa Cruz", zip: "95062" },
+  { name: "Honolulu", zip: "96803" },
+];
+class WeatherDisplay extends Component {
+  render() {
+    return <h1>Displaying Weather for city {this.props.zip}</h1>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {places.map((place, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              console.log("Clicked index " + index);
+            }}
+          >
+            {place.name}
+          </button>
+        ))}
+        <WeatherDisplay zip={"12345"} />
+      </div>
+    );
+  }
 }
 
 export default App;
